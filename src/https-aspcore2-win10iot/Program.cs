3cfd9f908b7371948e89d8a7025eab832a37d9ec
Tom Kuijsten <t.kuijsten@bronkhorst.com>
2017-09-25 14:47:26 +0200

    Created app

---
+++ src/https-aspcore2-win10iot/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Net;
+
+namespace https_aspcore2_win10iot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    options.Listen(
+                        IPAddress.Any,
+                        5000,
+                        listenOptions =>
+                        {
+                            listenOptions.UseHttps("testCertificate.pfx", "p@ssw0rd");
+                        }
+                    );
+                })
+                .Build();
+        }
+    }
+}
